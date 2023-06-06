6687936ce646ccfb012f997185a99758ef118db3
aleksizverev <sourcetree.comic278@aleeas.com>
2023-06-06 23:15:50 +0300

    another small test commit

--- PhotoGrammy/ImagesList/ImagesListViewController.swift
+++ PhotoGrammy/ImagesList/ImagesListViewController.swift
@@ -18,7 +18,6 @@ class ImagesListViewController: UIViewController {
         imageListTableView.delegate = self
         imageListTableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
-    
 }
 
 extension ImagesListViewController: UITableViewDataSource {

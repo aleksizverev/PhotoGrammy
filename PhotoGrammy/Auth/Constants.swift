1d276cacdf403e1eb413363f31fc1e1d5ac03836
aleksizverev <sourcetree.comic278@aleeas.com>
2023-07-18 15:07:40 +0300

    sprint_10 after review

---
+++ PhotoGrammy/Auth/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "MCaK5u5OpfFpJLHuDnGMzxCtkNE3E2ZAT0L1TnXwy9k"
+let SecretKey = "yzMpEYwa2MbjpTDnMaWzJLPoqgPiFHLzWSQ-Z47m2wA"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
